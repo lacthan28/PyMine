7e717fdd77a48c7705a7961dd8bfa10ffbb44052
lacthan28 <lacthan28@gmail.com>
2017-05-31 16:32:10 -0700

    update Worker class

--- src/pymine/Worker.py
+++ src/pymine/Worker.py
@@ -1,20 +1,18 @@
+from src.spl.stubs.Core import interface_exists
+from src.spl.stubs.pthreads import Worker
 from .Server import *
 from .ThreadManager import *
 import urllib
 from ..spl.ClassLoader import *
 
 
-class Worker:
+class Worker(Worker):
     classLoader = None
 
     isKilled = False
 
-    def check_connectivity(reference):
-        try:
-            urllib.request.urlopen(reference, timeout=1)
-            return True
-        except urllib.request.URLError:
-            return False
+    def __init__(self):
+        super(Worker,self).__init__()
 
     def getClassLoader(self):
         return self.classLoader
@@ -25,9 +23,9 @@ class Worker:
         self.classLoader = loader
 
     def registerClassLoader(self):
-        if not has_:
-            import src.spl.ClassLoader
-            import src.spl.BaseClassLoader
+        if not interface_exists("ClassLoader", False):
+            import spl.ClassLoader
+            import spl.BaseClassLoader
 
         if self.classLoader is not None:
             self.classLoader.register(True)
@@ -35,4 +33,10 @@ class Worker:
     def start(self, options = 0x111111):
         ThreadManager.getInstance().add(self)
 
+        if not self.isRunning() and not self.isJoined() and not self.isTerminated():
+            if self.getClassLoader() is None:
+                self.setClassLoader()
+
+            return Worker.start(options)
+        return False
         
